aa17a040af49aaa9cd7650d81e922af1cb87a799
Bob Park <hwpark11@kakao.com>
2025-01-25 14:05:58 +0900

    Initialize project

---
+++ src/test/java/com/malgn/ontime/OnTimeWebServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.malgn.ontime;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnTimeWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
