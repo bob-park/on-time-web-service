1dd0e1c990ef8efb730c191971c787b57b7489a4
Bob Park <hwpark11@kakao.com>
2025-01-29 19:59:29 +0900

    feat: 근무 기록 조회 api 연동

--- src/main/java/com/malgn/ontime/domain/attendance/controller/AttendanceRecordController.java
+++ src/main/java/com/malgn/ontime/domain/attendance/controller/AttendanceRecordController.java
@@ -1,24 +1,33 @@
 package com.malgn.ontime.domain.attendance.controller;
 
+import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.malgn.ontime.domain.attendance.model.AttendanceRecordResponse;
+import com.malgn.ontime.domain.attendance.model.GetAttendanceRecordRequest;
 import com.malgn.ontime.domain.attendance.model.RecordAttendanceRequest;
 import com.malgn.ontime.domain.attendance.service.AttendanceRecordService;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("attendance/record")
+@RequestMapping("attendance/records")
 public class AttendanceRecordController {
     private final AttendanceRecordService attendanceRecordService;
 
     @PostMapping(path = "")
-    public AttendanceRecordResponse recordAttendance(@RequestBody RecordAttendanceRequest recordRequest){
+    public AttendanceRecordResponse recordAttendance(@RequestBody RecordAttendanceRequest recordRequest) {
         return attendanceRecordService.recordAttendance(recordRequest);
+    }
+
+    @GetMapping(path = "")
+    public List<AttendanceRecordResponse> getAllRecords(GetAttendanceRecordRequest getRequest) {
+        return attendanceRecordService.getAllRecord(getRequest);
     }
 }
